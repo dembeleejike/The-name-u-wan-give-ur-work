52c0cc4ec446a1bc1d4d866914e82d1840e7ae74
dembeleejike <eugeneejike51@gmail.com>
2025-10-15 20:11:25 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+console.log('Hello World!');
+let cartQuantity = 0;
+console.log(cartQuantity);
+let randomNumber = Math.random();
+console.log(randomNumber);
+let computerMove = '';
+console.log(computerMove);
+let result = '';
+console.log(result);
+      
